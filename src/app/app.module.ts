ed120a11b6bf6b15f841d566342086025beac3ab
dinith72 <jdinith72@gmail.com>
2019-01-06 18:23:30 +0530

    made some changes to the validation when submit clicked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 
-import { 
+import {
           MatButtonModule ,
           MatInputModule ,
           MatIconModule,
