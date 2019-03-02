afbaaa18850b3c28fa9b1cb9a9163f5e10536213
dinith72 <jdinith72@gmail.com>
2019-03-02 11:02:54 +0530

    change the location of the image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,36 @@
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { environment } from './../environments/environment';
-import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule , ReactiveFormsModule} from '@angular/forms';
-import { NgModule } from '@angular/core';
-import {RouterModule} from '@angular/router';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HttpClientModule} from '@angular/common/http';
-import { AngularFireModule  } from '@angular/fire';
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { environment } from "./../environments/environment";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { AngularFireModule } from "@angular/fire";
 // import {AngularFireDatabaseModule} from '@angular/fire/database';
 
 import {
-          MatButtonModule ,
-          MatInputModule ,
-          MatIconModule,
-          MatSelectModule,
-          MatOptionModule,
-          MatCheckboxModule,
-          MatFormFieldModule,
-          MatGridListModule,
-          MatCardModule,
-          MatSnackBarModule,
-     } from '@angular/material';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatCardModule,
+  MatSnackBarModule
+} from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // importing the compinnts
-import { MainFormComponent } from './main-form/main-form.component';
-import { from } from 'rxjs';
+import { MainFormComponent } from "./main-form/main-form.component";
+import { from } from "rxjs";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainFormComponent
-  ],
+  declarations: [AppComponent, MainFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -51,11 +48,10 @@ import { from } from 'rxjs';
     MatCardModule,
     MatSnackBarModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase),
-
-
+    // MatDrawerContainer,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
